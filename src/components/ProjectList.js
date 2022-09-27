ee8e0a924ef5ccda97456c23792fda4aa485bd6b
ray omego <ray.omego@student.moringaschool.com>
2022-09-27 13:46:30 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  cconsole.log(projects);
+  console.log(projects);
 	return (
 		<div id="projects">
 			<h2>My Projects</h2>
